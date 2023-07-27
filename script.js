c4344e301f03fa2bd1ae889402f404825d10c7e8
abigailmgarcia <abigailmg31@gmail.com>
2023-07-27 00:47:10 -0500

    added function to call api by city

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 const apiKey = "9968025b614fb3b8cb40954de8d356b1";
+
+// Global Variables
+const sumbitButton = document.getElementById('sumbit-button');
+const getCoordinates = function() {
+    const searchCities = document.querySelector('#search-bar').value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCities}&appid=${apiKey}`)
+    .then(function (response) {
+        console.log(response.JSON);
+    });
+};
+
+sumbitButton.addEventListener("click", getCoordinates);
