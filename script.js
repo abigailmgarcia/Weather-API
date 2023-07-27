033a5824eee5c57f1ad9d5f40dfe0ec84e0df490
abigailmgarcia <abigailmg31@gmail.com>
2023-07-27 03:15:58 -0500

    added styling for page

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
-document.addEventListener("DOMContentLoaded", function(){
-const apiKey = "9968025b614fb3b8cb40954de8d356b1";
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = "9968025b614fb3b8cb40954de8d356b1";
 
-// Global Variables
-var sumbitButton = document.getElementById('sumbitBtn');
-console.log("Button Element:", document.getElementById('submitBtn'));
-let currentCondition;
-let cityName;
-let currentIcon;
-let currentDate;
-let currentIconUrl;
-let currentWindSpeed;
-let currentHumidity;
-let currentTemp;
-
-
-// // function to format date
-// const formatDate = function(date) {
-//     return dayjs(date).format("MM/DD/YYYY");
-// };
+    // Global Variables
+    var sumbitButton = document.getElementById('sumbitBtn');
+    console.log("Button Element:", document.getElementById('submitBtn'));
+    let currentCondition;
+    let cityName;
+    let currentIcon;
+    let currentDate;
+    let currentIconUrl;
+    let currentWindSpeed;
+    let currentHumidity;
+    let currentTemp;
 
 
-// function to get weather by city
-const getCoordinates = function() {
-    const searchCities = document.querySelector('#search-bar').value;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCities}&appid=${apiKey}`)
-    .then(function (response) {
-       return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        console.log(data.coord.lat, data.coord.lon);
-        const cityLat = data.coord.lat;
-        const cityLon = data.coord.lon;
-        weatherCondition(cityLat, cityLon);
-    })
-    .catch(function (error) {
-        console.error('error featching weather data:', error);
-    });
-};
+    // // function to format date
+    // const formatDate = function(date) {
+    //     return dayjs(date).format("MM/DD/YYYY");
+    // };
 
-    const weatherCondition = function(cityLat, cityLon) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            currentIcon = data.list[0].weather[0].icon;
-            currentIconUrl =`http://openweathermap.org/img/wn/${currentIcon}.png`
-            currentDate = dayjs().format("MM/DD/YYYY");
-            cityName = data.city.name;
-            currentTemp = data.list[0].main.temp;
-            currentCondition = data.list[0].weather[0].description;
-            currentWindSpeed = data.list[0].wind.speed;
-            currentHumidity = data.list[0].main.humidity;
-            console.log(data.list[0].main.humidity);
-            displayWeather(cityName, currentTemp, currentWindSpeed, currentHumidity);
-        });
+
+    // function to get weather by city
+    const getCoordinates = function () {
+        const searchCities = document.querySelector('#search-bar').value;
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchCities}&appid=${apiKey}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                console.log(data.coord.lat, data.coord.lon);
+                const cityLat = data.coord.lat;
+                const cityLon = data.coord.lon;
+                weatherCondition(cityLat, cityLon);
+            })
+            .catch(function (error) {
+                console.error('error featching weather data:', error);
+            });
     };
 
-    const displayWeather = function(cityName, currentTemp, currentWindSpeed, currentHumidity) {
+    const weatherCondition = function (cityLat, cityLon) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${apiKey}&units=imperial`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                currentIcon = data.list[0].weather[0].icon;
+                currentIconUrl = `http://openweathermap.org/img/wn/${currentIcon}.png`
+                currentDate = dayjs().format("MM/DD/YYYY");
+                cityName = data.city.name;
+                currentTemp = data.list[0].main.temp;
+                currentCondition = data.list[0].weather[0].description;
+                currentWindSpeed = data.list[0].wind.speed;
+                currentHumidity = data.list[0].main.humidity;
+                console.log(data.list[0].main.humidity);
+                displayWeather(cityName, currentTemp, currentWindSpeed, currentHumidity);
+            });
+    };
+
+    const displayWeather = function (cityName, currentTemp, currentWindSpeed, currentHumidity) {
         document.querySelector('currentIcon').setAttribute('src', currentIconUrl);
         document.getElementById('currentIcon').setAttribute('style', 'width: 10rem; height: 10rem')
         document.querySelector('cityName').textContent = "Current City:" + cityName;
@@ -70,8 +70,8 @@ const getCoordinates = function() {
         document.querySelector('currentHumidity').textContent = "Humidity: " + currentHumidity + "percent";
     }
 
-sumbitButton.addEventListener("click", function(){
-    getCoordinates();
-});
+    sumbitButton.addEventListener("click", function () {
+        getCoordinates();
+    });
 
 });
